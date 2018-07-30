04e3a9597e4d1bb7905dd9229ea189341fbc77b7
sburns18 <sburns18@holynames-sea.org>
2018-07-30 17:10:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-# dog.rb
+class Dog 
+  
+  def initialize(bark, sit, person, talk, walk )
+    @bark = bark
+    @sit = sit 
+    @person = person
+    @talk = talk 
+    @walk = walk 
+  end 
+end
+
+dog_one = Dog.new("Woof!", "The Dog is sitting", "Hello World!", "The Person is walking")
